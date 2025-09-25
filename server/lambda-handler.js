d890e57f0ce7a5e390ca49060954b190c210c905
Philip Ellis <philip.ellis@sailpoint.com>
2025-09-25 16:31:50 -0400

    added more logging

--- server/lambda-handler.js
+++ server/lambda-handler.js
@@ -1,13 +1,17 @@
 const serverlessExpress = require('@vendia/serverless-express');
 
 // Import the compiled JavaScript version from dist directory
-const app = require('./dist/web-api.js').default;
+const webApiModule = require('./dist/web-api.js');
+const app = webApiModule.default || webApiModule;
 
 if (!app) {
   throw new Error('Failed to import Express app from compiled web-api module');
 }
 
 console.log('Express app imported successfully for Lambda handler');
+console.log('App type:', typeof app);
+console.log('App constructor:', app.constructor ? app.constructor.name : 'no constructor');
+console.log('App listen method exists:', typeof app.listen);
 
 // Export the Lambda handler
 exports.handler = serverlessExpress(app);
